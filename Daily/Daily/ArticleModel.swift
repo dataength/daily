1da6ae646d3d14c5c7cc936a54d16e3fb2191131
Gatuk Chattanon <gatukgl@gmail.com>
2021-10-09 10:51:01 +0700

    Design the layout for articles :baby_chick: :bear:

---
+++ Daily/Daily/ArticleModel.swift
@@ -0,0 +1,7 @@
+struct ArticleModel: Codable {
+    let id: Int
+    let name: String
+    let author: String
+    let source: String
+    let imageUrl: String
+}
--- Daily/Daily/ContentView.swift
+++ Daily/Daily/ContentView.swift
@@ -9,8 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Data Eng News")
+            .font(.headline)
+
+        List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+            VStack(alignment: .leading) {
+                Text("What is a Data Warehouse?").font(.title2).padding(.bottom, 2.0)
+                Text("Unclear what a data warehouse is or when to use one? Then this post is for you. In this post, we go over what a data warehouse is, the need for it, and the differences between using an OLTP and OLAP database as a data warehouse.")
+                    .foregroundColor(.gray).padding(.bottom, 2.0)
+                HStack {
+                    Text("Start Data Engineering").foregroundColor(.gray)
+                    Text("Oct 3, 2021").foregroundColor(.gray)
+                }
+            }
+            .padding([.top, .leading, .trailing], 8.0)
+        }
     }
 }
 
