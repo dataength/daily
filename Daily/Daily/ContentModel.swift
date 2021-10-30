09236d0728e953e3f98b69da831755866a2651b5
Gatuk Chattanon <gatukgl@gmail.com>
2021-10-30 19:34:02 +0700

    Format date to be more readable

--- Daily/Daily/ContentModel.swift
+++ Daily/Daily/ContentModel.swift
@@ -8,10 +8,27 @@
 import Foundation
 
 struct ContentModel: Codable, Identifiable {
-    var id = UUID()
+    let id = UUID()
     let title: String
     let author: String
     let url: String
     let datetime: String
     let summary: String
+
+    var formattedDateTime: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        let date = dateFormatter.date(from: self.datetime)
+
+        let format = date?.getFormattedDate(format: "MMM d, yyyy") ?? ""
+        return format
+    }
+}
+
+extension Date {
+    func getFormattedDate(format: String) -> String {
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = format
+        return dateformat.string(from: self)
+    }
 }
