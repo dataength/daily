b2257d3846a0ce029078d78d59193bfff77aca30
Gatuk Chattanon <gatukgl@gmail.com>
2021-10-31 09:10:09 +0700

    Need to add UI test file back because it all tests cannot be built if don't have this

---
+++ Daily/DailyUITests/DailyUITests.swift
@@ -0,0 +1,25 @@
+//
+//  DailyUITests.swift
+//  DailyUITests
+//
+//  Created by Gatuk Chattanon on 31/10/21.
+//
+
+import XCTest
+
+class DailyUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
